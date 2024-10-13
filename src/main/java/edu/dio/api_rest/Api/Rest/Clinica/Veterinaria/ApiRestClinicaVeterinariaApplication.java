f86dcb0ad36bdfe0f486a6afb0127bf614bc7eb1
Catia Jorge <catiarjs20@gmail.com>
2024-10-12 23:38:55 -0300

    Criação do Projeto Api Rest

---
+++ src/main/java/edu/dio/api_rest/Api/Rest/Clinica/Veterinaria/ApiRestClinicaVeterinariaApplication.java
@@ -0,0 +1,13 @@
+package edu.dio.api_rest.Api.Rest.Clinica.Veterinaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestClinicaVeterinariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestClinicaVeterinariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/dio/api_rest/Api/Rest/Clinica/Veterinaria/ApiRestClinicaVeterinariaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dio.api_rest.Api.Rest.Clinica.Veterinaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestClinicaVeterinariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
